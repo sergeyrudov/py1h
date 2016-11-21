c31fada4f41736afe095877efd45ccc9f3802879
sergey <m3quattro@mail.ru>
2016-11-21 10:07:15 +0200

    123

---
+++ dice.py
@@ -0,0 +1,20 @@
+money_player1 = 100
+money_player2 = 100
+
+def get_rate():
+    while True:
+        print('Player 1 сделай свою ставку')
+        stavka_player1 = int(input())
+        if stavka_player1 > money_player1:
+            print('Нехватает баланса')
+            continue
+        print('Player 2 сделай свою ставку')
+        stavka_player2 = int(input())
+        if stavka_player2 > money_player2:
+            print('не хватает денег')
+            continue
+        if stavka_player1 != stavka_player2:
+            print('Ставка должна быть одинакова для игроков')
+            continue
+        return stavka_player1
+get_rate()
