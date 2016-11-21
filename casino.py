3c8a22875d091ef080446afc47a60d4d2a14de95
sergey <m3quattro@mail.ru>
2016-11-21 09:19:49 +0200

    123

--- casino.py
+++ casino.py
@@ -1,23 +1,28 @@
 from random import randint
 money = 100
 
-def get_result(num):
+def get_result(num, rate):
+    global money
     r = randint(1,9)
     if num == r:
         print('ugadal')
     else:
         print('neugadal')
     if num !=r:
-            print (money-num)
+        money = money-rate
     else:
-        print(money*3)
-
-
+        money = money + rate *3
+    print ('На вашем балансе: {0} грн'.format(money))
 
 def main():
-    while True:
+    while money !=0:
         print('Вывести число от 1 до 9')
         num = int(input())
-        get_result(num)
+        print ('Сделайте свою ставку')
+        rate = int(input())
+        if rate > money:
+            print('Ставка превышает баланс!')
+            continue
+        get_result(num, rate)
 
 print(main())
