c1c2f7c123f638261720a83431566bd18c692657
sergey <m3quattro@mail.ru>
2017-01-12 10:38:24 +0200

    test zadanie

--- dzsreda.py
+++ dzsreda.py
@@ -4,4 +4,8 @@ def filework (path1, path2):
     for element in range(len(f1)):
         print(f1[element])
 
-filework(r'd:/test/text1.txt',r'd:/test/text2.txt')
+#filework(r'd:/test/text1.txt',r'd:/test/text2.txt')
+
+password1 = 'Elementary'
+password1 = hashlib.sha512(password1.encode('utf-8')).hexdigest()
+print (password1)
